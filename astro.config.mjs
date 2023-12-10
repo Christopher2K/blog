b19d25febfa73977998f634f5f8086578b31886b
Christopher N. KATOYI KABA <me@christopher2k.dev>
2023-12-10 12:15:38 -0500

    feat: install solidjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
 import pandacss from "@pandacss/astro";
+import solidJs from "@astrojs/solid-js";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [pandacss()],
+  integrations: [pandacss(), solidJs()]
 });
