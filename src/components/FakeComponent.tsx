47f3a7a76ef66cbd38e852cb0e3e77bb56c6d8de
Christopher N. KATOYI KABA <me@christopher2k.dev>
2023-12-10 13:42:35 -0500

    feat: install i18n solution

--- src/components/FakeComponent.tsx
+++ src/components/FakeComponent.tsx
@@ -1,4 +1,5 @@
 import { onCleanup, createSignal } from "solid-js";
+import { t } from "astro-i18n";
 
 export const FakeComponent = () => {
   const [count, setCount] = createSignal(0);
@@ -7,5 +8,9 @@ export const FakeComponent = () => {
 
   onCleanup(() => clearInterval(interval));
 
-  return <div>Count value is {count()}</div>;
+  return (
+    <div>
+      Count value is {count()} {t("your_common")}
+    </div>
+  );
 };
