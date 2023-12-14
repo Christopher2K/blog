941673deed8e883373bf498d21038715a38bed9d
Christopher N. KATOYI KABA <me@christopher2k.dev>
2023-12-14 18:53:03 -0500

    feat: add mdx and images

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from "astro/config";
 import pandacss from "@pandacss/astro";
 import solidJs from "@astrojs/solid-js";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [pandacss(), solidJs()],
+  integrations: [pandacss(), solidJs(), mdx()]
 });
