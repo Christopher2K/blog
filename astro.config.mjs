309fc8dac3996a13368b5aa08cff94f442af3abb
Christopher N. KATOYI KABA <me@christopher2k.dev>
2024-05-31 18:39:15 -0400

    fix: centered title

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from "astro/config";
-import pandacss from "@pandacss/astro";
 import solidJs from "@astrojs/solid-js";
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [pandacss(), solidJs(), mdx()],
+  integrations: [solidJs(), mdx()],
   i18n: {
     defaultLocale: "fr",
     locales: ["en", "fr"],
