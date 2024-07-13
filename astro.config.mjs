b6f5e54407ed9c15252479af15985ca9ddabf9e6
Christopher N. KATOYI KABA <me@christopher2k.dev>
2024-07-13 17:41:29 -0400

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,25 @@
 import { defineConfig } from "astro/config";
 import solidJs from "@astrojs/solid-js";
-
 import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://christopher.engineering",
-  integrations: [solidJs(), mdx()],
+  integrations: [
+    solidJs(),
+    mdx(),
+    sitemap({
+      i18n: {
+        defaultLocale: "fr",
+        locales: {
+          fr: "fr-FR",
+          en: "en-US",
+        },
+      },
+    }),
+  ],
   i18n: {
     defaultLocale: "fr",
     locales: ["en", "fr"],
@@ -15,3 +28,4 @@ export default defineConfig({
     },
   },
 });
+
