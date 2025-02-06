9d978f3d81e7cf0706c708e75e70be8e8267d57b
Christopher N. KATOYI KABA <me@christopher2k.dev>
2025-02-06 18:44:24 -0500

    feat: expo live activity article

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 import { defineConfig } from "astro/config";
 import solidJs from "@astrojs/solid-js";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://christopher.engineering",
   integrations: [
     solidJs(),
+    expressiveCode({
+      theme: "catppuccin-mocha",
+    }),
     mdx(),
     sitemap({
       i18n: {
